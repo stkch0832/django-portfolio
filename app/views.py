2d9f42458ef51e80045895ffa54afc523d3e56b9
stkch0832 <stkch0832@gmail.com>
2022-06-28 21:39:01 +0900

    create index.html
    add urls
    add view IndexView
    create base.html
    create index.html

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import Profile
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        profile_data = Profile.objects.all()
+        if profile_data.exists():
+            profile_data = profile_data.order_by("-id")[0]
+        return render(request, 'app/index.html', {
+            'profile_data': profile_data,
+        })
