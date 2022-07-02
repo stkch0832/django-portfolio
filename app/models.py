6418c60e4512f2158d2be9429cc4045487a2fed2
stkch0832 <stkch0832@gmail.com>
2022-07-02 21:16:56 +0900

    complete

--- app/models.py
+++ app/models.py
@@ -17,3 +17,53 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+class Work(models.Model):
+    title = models.CharField('タイトル', max_length=100)
+    image = models.ImageField(upload_to='images', verbose_name='イメージ画像')
+    thumbnail = models.ImageField(upload_to='images', verbose_name='サムネイル', null=True, blank=True)
+    skill = models.CharField('スキル', max_length=100)
+    url = models.CharField('URL', max_length=100, null=True, blank=True)
+    created = models.DateField('作成日時')
+    description = models.TextField('説明')
+
+    def __str__(self):
+        return self.title
+
+
+class Experience(models.Model):
+    occupation = models.CharField('職種', max_length=100)
+    company = models.CharField('会社', max_length=100)
+    description = models.TextField('説明')
+    place = models.CharField('場所', max_length=100)
+    period = models.CharField('期間', max_length=100)
+
+    def __str__(self):
+        return self.occupation
+
+
+class Education(models.Model):
+    course = models.CharField('コース', max_length=100)
+    school = models.CharField('学校', max_length=100)
+    place = models.CharField('場所', max_length=100)
+    period = models.CharField('期間', max_length=100)
+
+    def __str__(self):
+        return self.course
+
+class Software(models.Model):
+    name = models.CharField('ソフトウェア', max_length=100)
+    level = models.CharField('レベル', max_length=100)
+    percentage = models.IntegerField('パーセンテージ')
+
+    def __str__(self):
+        return self.name
+
+
+class Technical(models.Model):
+    name = models.CharField('テクニカル', max_length=100)
+    level = models.CharField('レベル', max_length=100)
+    percentage = models.IntegerField('パーセンテージ')
+
+    def __str__(self):
+        return self.name
